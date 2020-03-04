a4dd29d99f41ee871585859c3723e8584acf6257
vldplcd <shilov.part@gmail.com>
2020-03-04 13:36:26 +0300

    JSON serializable

--- UnicornStructure.py
+++ UnicornStructure.py
@@ -1,4 +1,5 @@
 from abc import ABC, abstractmethod
+import json
 
 
 class ModelElement(ABC):
@@ -7,24 +8,28 @@ class ModelElement(ABC):
     name = "NA"
     description = "NA"
 
-    def __init__(self, name):
-        self.name = name
-        self.object_id = hash(name) // 10
+    def __init__(self, data: str):
+        self.__dict__ = json.loads(data)
+
+    def to_json(self):
+        return json.dumps(self, default=lambda o: o.__dict__,
+                          sort_keys=True, indent=4)
 
 
 class Anchor(ModelElement):
 
     attributes = []
 
-    def __init__(self, name, attributes):
-        self.name = name
-        self.object_id = hash(name) // 10
-        self.attributes = attributes
+    def __init__(self, data: str):
+        self.__dict__ = json.loads(data)
+        temp_attr = self.attributes.copy()
+        self.attributes = []
+        for attr in temp_attr:
+            self.attributes.append(Attribute(json.dumps(attr)))
 
 
 class Attribute(ModelElement):
-    type = "NA"
-    parent_anchor = Anchor()
+    value_type = "NA"
     parent_anchor_id = -2
     is_historized = False
 
@@ -33,25 +38,28 @@ class Tie(ModelElement):
     relations = []  #
     is_historized = False
 
-    def __init__(self, name, relations):
-        self.object_id = hash(name) // 10
-        self.relations = relations
-
+    def __init__(self, data: str):
+        self.__dict__ = json.loads(data)
 
 class Knot(ModelElement):
     connected_elements = []
 
-    def __init__(self, name):
-        self.object_id = hash(name) // 10
 
-
-class Model:
+class Model(object):
     anchors = []
     ties = []
     knots = []
 
+    def __init__(self, data: str):
+        data_dict = json.loads(data)
+        for anch in data_dict['anchors']:
+            self.anchors.append(Anchor(json.dumps(anch)))
 
-class Relation:
+        for tie in data_dict['ties']:
+            self.anchors.append(Tie(json.dumps(tie)))
+
+
+class Relation(object):
     name = ""
     type = ""  # N;1
-    anchor = Anchor()
+    anchor_id = -1
